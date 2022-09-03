ba95ab032497dd4e1d4f42d9c03bd50d099f4cbe
Ryan de Jonge <ryandejonge@outlook.com>
2022-09-03 20:30:34 +0200

    Early stuff

---
+++ Axolotl2D/Axolotl2D.Sample/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Axolotl2D.Axolotl();
+game.Run();
---
+++ Axolotl2D/Axolotl2D/Interfaces/IScene.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axolotl2D.Interfaces
+{
+    public interface IScene
+    {
+        Task InitializeAsync();
+
+        Task OnUpdateAsync(GameTime gameTime);
+
+        Task OnFixedUpdateAsync(GameTime gameTime);
+
+        Task OnDrawAsync(GameTime gameTime);
+
+        Task DestroyAsync();
+    }
+}
